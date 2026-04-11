17e554ff6759da72cea001b39ffdbeb61ed0ca41
Eli-Saavedra <SaavedraEli737@gmail.com>
2026-04-11 17:45:18 +0000

    Added header grid instead of stackpanel

--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3a0503d1b3549c4ad80a702830dbea8c708406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee523cce59fa1b1a36e22cb5bc01622b5f98105a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
