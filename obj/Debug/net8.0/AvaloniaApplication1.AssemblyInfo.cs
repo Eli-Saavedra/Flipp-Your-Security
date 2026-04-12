aa7e22b68f3b7ab444b93ff1680f3732a9254648
Eli-Saavedra <SaavedraEli737@gmail.com>
2026-04-12 02:51:06 +0000

    Made border to go to the edge of the screens.

--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e554ff6759da72cea001b39ffdbeb61ed0ca41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcacde1faaf028346599343d19c27ca0e96a76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
